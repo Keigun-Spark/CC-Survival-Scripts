0e71d0b3ca5ecc7911d2d02d36660968bdadda46
Julian B <Keigun-Spark@outlook.com>
2022-10-08 22:07:46 +0200

    Reverted

--- scripts/mineCuboid.lua
+++ scripts/mineCuboid.lua
@@ -1,8 +1,8 @@
-local library = require("library")
+local libraryrary = require("libraryrary")
 local tArgs = {...}
 
 function mineSquence(width, height, depth, side)
-	
+	error("This code sucked so I removed it.")
 end
 
 if type(tonumber(tArgs[1])) ~= "number" and type(tonumber(tArgs[2])) ~= "number" and type(tonumber(tArgs[3])) ~= "number" and type(tostring(tArgs[4])) ~= "right" and type(tostring(tArgs[4])) ~= "left" then
@@ -11,10 +11,10 @@ if type(tonumber(tArgs[1])) ~= "number" and type(tonumber(tArgs[2])) ~= "number"
 	error("Width, height, depth and side are required! (Example: '5 5 10 right') [5 blocks wide, 5 block heigh, 10 blocks deep and to the right of turtle]")
 end
 
-local start = library.data.copyTable(library.data.coords)
-library.data.saveData("/.save", "/start_pos", start)
-library.storage.avoidChest()
+local start = libraryrary.data.copyTable(libraryrary.data.coords)
+libraryrary.data.saveData("/.save", "/start_pos", start)
+libraryrary.storage.avoidChest()
 mineSquence(tonumber(tArgs[1]), tonumber(tArgs[2]), tonumber(tArgs[3]), (tostring(tArgs[4])))
-library.move.moveTo(start.x, start.y, start.z)
-library.storage.drop(library.tools.maxSlots)
+libraryrary.move.moveTo(start.x, start.y, start.z)
+libraryrary.storage.drop(libraryrary.tools.maxSlots)
 fs.delete("/.save")
